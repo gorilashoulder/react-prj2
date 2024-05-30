961c37e50aae879ffce01517da2ce35cdefdd603
sunghwan <simcom0820@naver.com>
2024-05-30 15:12:25 +0900

    게시물 목록에 댓글 수 표시

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -20,6 +20,7 @@ import {
   faAngleRight,
   faAnglesLeft,
   faAnglesRight,
+  faComments,
   faHeart,
   faImage,
   faMagnifyingGlass,
@@ -103,6 +104,12 @@ export function BoardList() {
                       <Badge>
                         <FontAwesomeIcon icon={faImage} />
                         {board.numberOfImages}
+                      </Badge>
+                    )}
+                    {board.numberOfComments > 0 && (
+                      <Badge>
+                        <FontAwesomeIcon icon={faComments} />
+                        {board.numberOfComments}
                       </Badge>
                     )}
                   </Td>
